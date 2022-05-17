a6d057e93543d18d2ab0cf37efc288404e530996
Gabriel <gabrielpacirnediniz@gmail.com>
2022-05-16 22:28:50 -0300

    Creating the widget and the types of choices

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// Componente é uma função que retorna HTML
+// Sempre deve ter Primeira letra maiúscula e mesmo nome no arquivo
+// Propriedades funcionam da mesma forma do HTML
+import { Widget } from "./components/Widget"
+
+export function App() {
+  return <Widget />
+}
